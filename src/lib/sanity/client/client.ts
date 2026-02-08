9fa63ddd7fc829f3f3ab4027e251992fccbbc78e
Farad Alam <farad.dev@gmail.com>
2026-02-08 16:51:51 +0600

    Disable Sanity CDN for fresh data revalidation

--- src/lib/sanity/client/client.ts
+++ src/lib/sanity/client/client.ts
@@ -5,7 +5,7 @@ export const client = createClient({
   projectId: clientEnv.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: clientEnv.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: clientEnv.NEXT_PUBLIC_SANITY_API_VERSION,
-  useCdn: true,
+  useCdn: false,
   perspective: 'published',
   stega: {
     studioUrl: clientEnv.NEXT_PUBLIC_SANITY_STUDIO_URL,
